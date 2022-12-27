8c300e8e74710e47682e874159a50746856809cf
Cascaval Nadejda <vremea.dr@gmail.com>
2022-12-27 19:20:33 +0000

    Banner

--- src/components/FooterBanner.jsx
+++ src/components/FooterBanner.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+//img
+import headphones from '../assets/img/headphones_banner.webp'
+//scss
+import '../assets/style/footerBanner.scss';
 
 export default function FooterBanner() {
   return (
-    <div>FooterBanner</div>
+    <div className='footerBanner'>
+
+      <div className="left">
+        <p className='small'>25% off</p>
+        <p className="large">smile</p>
+        <p className="large">fine</p>
+      </div>
+
+      <div className="right">
+        <p className="small">Best choise</p>
+        <p className="medium">winter sale</p>
+        <p className="small">Great headphones on the market</p>
+        <button className='btn'>Buy now</button>
+      </div>
+
+      <img src={headphones} alt="headphones" />
+    </div>
   )
 }
